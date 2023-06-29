39b0066bfa3f661c5cf4ca72ce50f9ddb823ba15
collins-ruto <collinsruto48@gmail.com>
2023-06-29 15:23:07 +0300

    ¯\_(ツ)_/¯

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     # path('login/', views.login_user, name='login'),
     path("logout/", views.logout_user, name="logout"),
+    path("register/", views.register_user, name="register"),
     path("add_record/", views.add_record, name="add_record"),
     path("record/<int:p_k>", views.customer_record, name="record"),
     path("delete_record/<int:p_k>", views.delete_record, name="delete_record"),
