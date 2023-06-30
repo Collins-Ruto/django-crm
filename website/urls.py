cefb78426f5b869e2b60e819c1827916bc3827a9
collins-ruto <collinsruto48@gmail.com>
2023-07-01 01:49:09 +0300

    pages

--- website/urls.py
+++ website/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path("", views.home, name="home"),
     # path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
-    path('register/', views.register_user, name='register'),
+    path("logout/", views.logout_user, name="logout"),
+    path("add_record/", views.add_record, name="add_record"),
+    path("record/<int:p_k>", views.customer_record, name="record"),
+    path("delete_record/<int:p_k>", views.delete_record, name="delete_record"),
 ]
